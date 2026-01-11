02b8005fd0a40c73bfa8b1644a3f54766dd0f7a6
mcouto0xml <murilo.oliveira@sou.inteli.edu.br>
2026-01-11 11:17:13 -0300

    feat: add app

---
+++ src/app/app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    # Cor vinda da variável de ambiente
+    color = os.getenv("APP_COLOR", "#2c3e50")  # fallback seguro
+    version = os.getenv("APP_VERSION", "dev")
+
+    return render_template(
+        "index.html",
+        background_color=color,
+        version=version
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
